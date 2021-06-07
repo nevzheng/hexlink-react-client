c6db9370de7965b95b465374dd89ded079542e1c
Nevin Zheng <nevzheng@gmail.com>
2021-06-07 19:40:50 -0400

    Made Table Collapsible

--- src/components/LinkTable.tsx
+++ src/components/LinkTable.tsx
@@ -3,35 +3,54 @@ import Table from "react-bootstrap/Table";
 import LinkTableRow from "./LinkTableRow";
 
 import { useAppContext } from "../context/AppContext";
+import { Collapse } from "react-bootstrap";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 const LinkTable: React.FC = () => {
   const { state } = useAppContext();
 
+  const [showPreviousResults, setShowPreviousResults] = useState(false);
+
   return (
     <div>
-      <h3>All Results</h3>
-      <p>
-        Links Not Persisted on Refresh. Would need to implement user or
-        anonymous sessions
-      </p>
-      <Table size="sm">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Full URL</th>
-            <th>Shortened</th>
-          </tr>
-        </thead>
-        <tbody>
-          {state.redirects != null
-            ? state.redirects.map((redirect, index) => {
-                return (
-                  <LinkTableRow redirect={redirect} key={index} index={index} />
-                );
-              })
-            : ""}
-        </tbody>
-      </Table>
+      <Button
+        className="mb-3"
+        onClick={() => setShowPreviousResults(!showPreviousResults)}
+        variant={showPreviousResults ? "warning" : "info"}
+        aria-controls="example-collapse-text"
+        aria-expanded={showPreviousResults}
+      >
+        {showPreviousResults
+          ? "Hide Previous Results"
+          : "Show Previous Results"}
+      </Button>
+      <Collapse in={showPreviousResults}>
+        <div id="example-collapse-text">
+          <Table size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Full URL</th>
+                <th>Shortened</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.redirects != null
+                ? state.redirects.map((redirect, index) => {
+                    return (
+                      <LinkTableRow
+                        redirect={redirect}
+                        key={index}
+                        index={index}
+                      />
+                    );
+                  })
+                : ""}
+            </tbody>
+          </Table>
+        </div>
+      </Collapse>
     </div>
   );
 };
